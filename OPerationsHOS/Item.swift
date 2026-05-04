c619f11f77fa9acb8475403aaf1f01724ea18cc4
Michael Fluharty <michael@fluharty.com>
2026-05-04 16:50:32 -0500

    Initial Commit

---
+++ OPerationsHOS/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  OPerationsHOS
+//
+//  Created by Michael Fluharty on 5/4/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ OPerationsHOSTests/OPerationsHOSTests.swift
@@ -0,0 +1,18 @@
+//
+//  OPerationsHOSTests.swift
+//  OPerationsHOSTests
+//
+//  Created by Michael Fluharty on 5/4/26.
+//
+
+import Testing
+
+struct OPerationsHOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
